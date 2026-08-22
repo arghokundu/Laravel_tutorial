a5a848f141178136a383148368de1ab1a1821b73
arghokundu <supriyokundu2003@gmail.com>
2026-08-22 15:09:03 +0530

    design done

--- resources/views/CRUD/ShowAllStdDetailsList.blade.php
+++ resources/views/CRUD/ShowAllStdDetailsList.blade.php
@@ -1,86 +1,92 @@
 @extends('boot_link_html')
 @section('main_content')
 <div class="container mt-4">
-    <h1 class="mb-4">Student List</h1>
-    <div class="d-flex justify-content-between">
-        <div class="mb-3">
-            <a href="/formShow" class="btn btn-primary">Add New</a>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="mb-4">Student List</h1>
+            <div class="d-flex justify-content-between">
+                <div class="mb-3">
+                    <a href="/formShow" class="btn btn-primary">Add New</a>
+                </div>
+
+                <form action="/showAllStudentList" method="get" class="mb-4">
+                    <div class="d-flex gap-2">
+                        <input type="text" name="search" id="searchName" class="form-control" style="width: 250px;">
+
+                        <button type="submit" class="btn btn-primary">
+                            Search
+                        </button>
+
+                        <a href="/showAllStudentList" class="btn btn-secondary">
+                            Reset
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
 
-        <form action="/showAllStudentList" method="get" class="mb-4">
-            <div class="d-flex gap-2">
-                <input type="text" name="search" id="searchName" class="form-control" style="width: 250px;">
-
-                <button type="submit" class="btn btn-primary">
-                    Search
-                </button>
-
-                <a href="/showAllStudentList" class="btn btn-secondary">
-                    Reset
-                </a>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped table-hover" id="listStd">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Sl.no</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone No</th>
+                            <th>Pin Code</th>
+                            <th>Address</th>
+                            <th>State</th>
+                            <th>District</th>
+                            <th>Subdivision</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($studentsDetails as $stdDtl)
+                        <tr>
+                            <td>
+                                {{ $studentsDetails->firstItem() + $loop->index }}
+                            </td>
+                            <td>{{ $stdDtl->Name }}</td>
+                            <td>{{ $stdDtl->Email }}</td>
+                            <td>{{ $stdDtl->phoneNo }}</td>
+                            <td>{{ $stdDtl->pin }}</td>
+                            <td>{{ $stdDtl->Address }}</td>
+                            <td>{{ $stdDtl->state->state_name }}</td>
+                            <td>{{ $stdDtl->district->district_name }}</td>
+                            <td>{{ $stdDtl->subdivision->subdiv_name }}</td>
+                            <td>
+                                <div class="d-flex gap-1">
+                                    <a href="/editData/{{ $stdDtl->student_id_pk }}" class="btn btn-sm btn-warning">
+                                        Edit
+                                    </a>
+                                    <a href="/specific/data/{{$stdDtl->student_id_pk }}" class="btn btn-sm btn-info">
+                                        View
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-        </form>
-    </div>
+        </div>
 
-    <div class="table-responsive" >
-        <table class="table table-bordered table-striped table-hover" id="listStd">
-            <thead class="table-dark">
-                <tr>
-                    <th>Sl.no</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone No</th>
-                    <th>Pin Code</th>
-                    <th>Address</th>
-                    <th>State</th>
-                    <th>District</th>
-                    <th>Subdivision</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($studentsDetails as $stdDtl)
-                <tr>
-                    <td>
-                        {{ $studentsDetails->firstItem() + $loop->index }}
-                    </td>
-                    <td>{{ $stdDtl->Name }}</td>
-                    <td>{{ $stdDtl->Email }}</td>
-                    <td>{{ $stdDtl->phoneNo }}</td>
-                    <td>{{ $stdDtl->pin }}</td>
-                    <td>{{ $stdDtl->Address }}</td>
-                    <td>{{ $stdDtl->state->state_name }}</td>
-                    <td>{{ $stdDtl->district->district_name }}</td>
-                    <td>{{ $stdDtl->subdivision->subdiv_name }}</td>
-                    <td>
-                        <div class="d-flex gap-1">
-                            <a href="/editData/{{ $stdDtl->student_id_pk }}" class="btn btn-sm btn-warning">
-                                Edit
-                            </a>
-                            <a href="/specific/data/{{$stdDtl->student_id_pk }}" class="btn btn-sm btn-info">
-                                View
-                            </a>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-
-    <!-- Pagination -->
-    <div class="d-flex justify-content-between align-items-center mt-3">
-        <div>
-            <!-- Showing
+        <!-- Pagination -->
+        <div class="d-flex justify-content-between align-items-center mt-3 me-4">
+            <div>
+                <!-- Showing
             {{--{{ $studentsDetails->firstItem() }}
             to
             {{ $studentsDetails->lastItem() }}
             of
             {{ $studentsDetails->total() }}--}}
             results -->
-        </div>
-        <div>
-            {{ $studentsDetails->links('pagination::bootstrap-5') }}
+            </div>
+            <div>
+                {{ $studentsDetails->links('pagination::bootstrap-5') }}
+            </div>
         </div>
     </div>
 </div>
--- resources/views/CRUD/editStdDetailsForm.blade.php
+++ resources/views/CRUD/editStdDetailsForm.blade.php
@@ -1,94 +1,111 @@
-<div>
-    <form action="/updateData/{{$studentcrud->student_id_pk}}" method="POST">
-        @csrf
-        <label>Name:</label>
-        <input type=text name="fullname" id="fullname" 
-            value="{{old('fullname',$studentcrud->Name ?? 'NA')}}"><br>
-        @error('fullname')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
+@extends('boot_link_html')
+@section('main_content')
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header d-flex justify-content-center">
+            <h2> Edit Student Details Form</h2>
+        </div>
+        <div class="card-body">
+            <form action="/updateData/{{$studentcrud->student_id_pk}}" method="POST">
+                @csrf
+                <div class="row">
+                    <div class="col-md-3">
+                        <label>Name:</label>
+                        <input type=text name="fullname" id="fullname" class="form-control" 
+                            value="{{old('fullname',$studentcrud->Name ?? 'NA')}}">
+                        @error('fullname')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
 
-        <label>Email:</label>
-        <input type=email name="email" id="email"
-            value="{{old('email',$studentcrud->Email ?? 'NA')}}"><br>
-        @error('email')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
+                    <div class="col-md-3">
+                        <label>Email:</label>
+                        <input type=email name="email" id="email" class="form-control"
+                             value="{{old('email',$studentcrud->Email ?? 'NA')}}">
+                        @error('email')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+                        <label>Address:</label>
+                        <input type=text name="address" id="address" class="form-control"
+                             value="{{old('address',$studentcrud->Address ?? 'NA')}}">
+                        @error('address')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+                        <label>Pin:</label>
+                        <input type=text name="pin" id="pin" class="form-control"
+                            value="{{old('pin',$studentcrud->pin ?? 'NA')}}">
+                        @error('pin')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
 
-        <label>Address:</label>
-        <input type=text name="address" id="address"
-            value="{{old('address',$studentcrud->Address ?? 'NA')}}"><br>
-        @error('address')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
+                    <div class="col-md-3">
+                        <label>Phone No:</label>
+                        <input type=text name="phoneno" id="phoneno" class="form-control"
+                            value="{{old('phoneno',$studentcrud->phoneNo ?? 'NA')}}">
+                        @error('phoneno')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+                        <label>State:</label>
+                        <select name="state" id="state" class="form-control">
+                            <option>-------Select State----------</option>
+                            @foreach($state as $st)
+                            <option value="{{$st->state_id_pk}}" 
+                                {{old('state',$studentcrud->state_id_fk) == $st->state_id_pk ? 'selected' : ''}}>
+                                {{$st->state_name}}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('state')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
 
-        <label>Pin:</label>
-        <input type=text name="pin" id="pin" 
-            value="{{old('pin',$studentcrud->pin ?? 'NA')}}"><br>
-        @error('pin')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
+                        <label>District:</label>
+                        <select name="district" id="district" class="form-control">
+                            <option>-------Select District----------</option>
+                            @foreach($district as $dis)
+                            <option value="{{$dis->district_id_pk}}"
+                                {{old('district',$studentcrud->district_id_fk) == $dis->district_id_pk ? 'selected' : ''}}>
+                                {{$dis->district_name}}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('district')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+                        <label>Subdivision:</label>
+                        <select name="subdivision" id="subdivision" class="form-control">
+                            <option>-------Select subdivision----------</option>
+                            @foreach($subdivision as $subdiv)
+                            <option value="{{$subdiv->subdiv_id_pk}}"
+                                {{old('subdivision',$studentcrud->subdiv_id_fk) == $subdiv->subdiv_id_pk ? 'selected' : ''}}>
+                                {{$subdiv->subdiv_name}}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('subdivision')
+                        <small class="text-danger">{{$message}}</small>
+                        @enderror
+                    </div>
+                </div>
 
-        <label>Phone No:</label>
-        <input type=text name="phoneno" id="phoneno" 
-            value="{{old('phoneno',$studentcrud->phoneNo ?? 'NA')}}"><br>
-        @error('phoneno')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
+                <button class="d-flex justify-content-center btn btn-warning mt-3" type="submit">
+                    Update
+                </button>
+            </form>
+        </div>
 
-        <label>State:</label>
-        <select name="state" id="state">
-            <option>-------Select State----------</option>
-            @foreach($state as $st)
-            <option value="{{$st->state_id_pk}}"
-                {{old('state',$studentcrud->state_id_fk) == $st->state_id_pk ? 'selected' : ''}}>
-                {{$st->state_name}}
-            </option>
-            @endforeach
-        </select><br><br>
-        @error('state')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
-
-        <label>District:</label>
-        <select name="district" id="district">
-            <option>-------Select District----------</option>
-            @foreach($district as $dis)
-            <option value="{{$dis->district_id_pk}}"
-                {{old('district',$studentcrud->district_id_fk) == $dis->district_id_pk ? 'selected' : ''}}>
-                {{$dis->district_name}}
-            </option>
-            @endforeach
-        </select><br>
-        @error('district')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
-
-        <label>Subdivision:</label>
-        <select name="subdivision" id="subdivision">
-            <option>-------Select subdivision----------</option>
-            @foreach($subdivision as $subdiv)
-            <option value="{{$subdiv->subdiv_id_pk}}"
-                {{old('subdivision',$studentcrud->subdiv_id_fk) == $subdiv->subdiv_id_pk ? 'selected' : ''}}>
-                {{$subdiv->subdiv_name}}
-            </option>
-            @endforeach
-        </select><br>
-        @error('subdivision')
-        <small>{{$message}}</small>
-        @enderror
-        <br>
-
-        <button type="submit">
-            Update
-        </button>
-    </form>
-
+    </div>
 </div>
+
+@endsection
